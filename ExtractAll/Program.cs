12c4296ae8df45128db33c9bf09098c0418f1d18
Akash Kava <39438041+ackava@users.noreply.github.com>
2025-02-06 14:54:18 +0530

    added certs

---
+++ ExtractAll/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ExtractAll;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var store = new X509Store(StoreName.Root, StoreLocation.LocalMachine);
+        store.Open(OpenFlags.ReadOnly);
+
+        if (store.Certificates.Count > 0)
+        {
+            var counter = 0;
+            foreach (var certificate in store.Certificates)
+            {
+                counter++;
+
+                var pem = certificate.ExportCertificatePem();
+                System.IO.File.WriteAllText("cert" + counter + ".crt", pem);
+            }
+        }
+    }
+}
